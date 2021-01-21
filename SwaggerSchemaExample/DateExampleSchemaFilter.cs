9ffc049125813ae9b3e63ab8a7f2b419c7febae0
Aris Liang <arisliang@navig8group.com>
2021-01-21 10:50:14 +0800

    init

---
+++ SwaggerSchemaExample/DateExampleSchemaFilter.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwaggerSchemaExample
+{
+    /// <summary>
+    /// Formats examples for OpenApi "date" format strings as <c>yyyy-MM-dd</c>.
+    /// </summary>
+    public class DateExampleSchemaFilter : ISchemaFilter
+    {
+        public void Apply(OpenApiSchema schema, SchemaFilterContext context)
+        {
+            // strongly-typed example values get serialized as JSON in ISO 8601 format, with undesired precision,
+            // so replace them with a string
+            if (schema.Format == "date" &&
+                schema.Example is OpenApiDate date)
+            {
+                schema.Example = new OpenApiString(date.Value.ToString("yyyy-MM-dd"));
+            }
+        }
+    }
+}
---
+++ SwaggerSchemaExample/WeatherForecastGet.cs
@@ -0,0 +1,18 @@
+﻿using Swashbuckle.AspNetCore.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwaggerSchemaExample
+{
+    public class WeatherForecastGet
+    {
+        /// <summary>
+        /// Date created. Format yyyy-MM-dd.
+        /// </summary>
+        [SwaggerSchema("The date it was created", Format = "date")]
+        public DateTime DateCreated { get; set; }
+    }
+}
